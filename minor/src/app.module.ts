9e15d79dfa43f2c7d5d582199bae7daed4fb17c4
Harsika Kumari <kumariharsika8@gmail.com>
2024-10-07 23:36:07 +0530

    connect database in type ORM

--- minor/src/app.module.ts
+++ minor/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { DatabaseController } from './user/controller/database/database.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
-  controllers: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '',
+      database: 'NestLearn',
+      synchronize: true,
+    }), ],
+  controllers: [DatabaseController],
   providers: [],
 })
 export class AppModule {}
